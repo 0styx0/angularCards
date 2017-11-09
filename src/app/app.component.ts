2086fe4eda6bf8f4f5100eaf74b688105c72edb6
DovidM <landyx47@gmail.com>
2017-11-09 16:40:12 -0500

    Remove unneeded function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,38 +5,13 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
   cards = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-  ngAfterViewInit() {
-
-    this.addPositioning();
-  }
-
-  addPositioning() {
-
-      const cards = document.getElementsByClassName('card') as any; // as any so can use for-of
-
-      const left = 5;
-      const top = 5;
-
-      let i = 0;
-
-      for (const card of cards) {
-
-        // card.style.top = `${top * i}%`;
-        card.style.left = `${left * i}%`;
-
-        i++;
-      }
-  }
 
   moveToTop(e: Event, i: number) {
 
     const cardToMove = this.cards.splice(i, 1)[0];
     this.cards.push(cardToMove);
-
-    this.addPositioning();
   }
 }
